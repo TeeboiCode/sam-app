1af796a7846af6dcf17f9a81f9f7c37ff12537a3
Nasirullahi <abdquadrinasirullahi@gmail.com>
2025-05-08 14:24:51 +0100

    first Hero-page update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid p-0">
+  <div style="width: 100%; min-width: 100%; margin: 0; padding: 0; border:1px solid black;">
     <linkeVue />
     <router-view />
   </div>
@@ -9,4 +9,8 @@
 import linkeVue from "./components/linksfiles.vue/linke.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+  /* .container-fluid{
+    width: 100%;
+  } */
+</style>
